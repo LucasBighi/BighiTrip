9cc004044932b8845bf297525d30edf7712fd1df
Lucas Marques Bighi <lucasbighi@air-de-lucas>
2018-10-05 00:16:52 -0300

    Initial Commit

---
+++ BighiTrip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BighiTrip
+//
+//  Created by Lucas Marques Bighi on 05/10/2018.
+//  Copyright © 2018 Lucas Marques Bighi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
